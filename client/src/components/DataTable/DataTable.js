fa15c649f1fa3a4f4b436ad0e6896ca81185f3db
Viktor Makarskyy <vmakarskyy@gmail.com>
2019-08-19 18:54:16 -0500

    Style DataTable

--- client/src/components/DataTable/DataTable.js
+++ client/src/components/DataTable/DataTable.js
@@ -1,7 +1,28 @@
 import React, { Component } from 'react';
 import './DataTable.css';
 
+
 const JsonTable = require('ts-react-json-table');
+
+var columnsJSON = [
+   {key: 'id', label: 'id'},
+   {key: 'first_name', label: 'First Name'},
+   {key: 'last_name', label: 'Last Name'},
+   {key: 'company', label: 'Company'},
+   {key: 'email', label: 'Email'},
+   {key: 'address1', label: 'Address1'},
+   {key: 'address2', label: 'Address2'},
+   {key: 'zip', label: 'Zip'},
+   {key: 'city', label: 'City'},
+   {key: 'state_long', label: 'State Long'},
+   {key: 'state', label: 'State'},
+   {key: 'phone', label: 'Phone'},
+  ];
+
+  var excludeColumns = [
+   'id',
+];
+
 
 class DataTable extends Component {
   constructor() {
@@ -12,15 +33,25 @@ class DataTable extends Component {
   }
 
   componentDidMount() {
-    fetch('/api/myJson')
+
+    fetch('/api/myJSON')
       .then(res => res.json())
-      .then(source => this.setState({source}, () => console.log('Json data fetched...', source)));
+      .then(source => this.setState({source}, () => console.log('JSON Data...', source)));
+    
   }
+
 
   render() {
     return (
-      <div>
-        <JsonTable rows={ this.state.source } />       
+      <div className='DataTable-container'>
+        <JsonTable 
+          className='mainTable' 
+          theadClassName={'headers'}
+          rows={ this.state.source } 
+          columns={ columnsJSON } 
+          excludeColumns={ excludeColumns }
+
+          />       
       </div>
     );
   }
