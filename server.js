6efdd354243eeb916f1aae9fcd68184eb1c58faf
Viktor Makarskyy <vmakarskyy@gmail.com>
2019-08-19 17:47:52 -0500

    Print CSV to JSON

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const express = require('express');
+const CSVToJSON = require("csvtojson");
+const FileSystem = require("fs");
 
 const app = express();
+
+CSVToJSON().fromFile("./csv/normal.csv").then(source => {
+	console.log(source);
+}); 
 
 app.get('/api/customers', (req, res) => {
   const customers = [
